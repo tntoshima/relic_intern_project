c0fd3f240e702e6e250f439e029e38ae2bb82e5f
yamanaka <ao04to02@gmail.com>
2023-08-23 17:23:33 +0900

    create db

---
+++ database/migrations/2023_08_23_160906_create_attendance_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(!Schema::hasTable('attendance')){
+            Schema::create('attendance', function (Blueprint $table) {
+                $table->id();
+                $table->integer('club_id')->unsigned();
+                $table->integer('member_id')->unsigned();
+                $table->datetime('date');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('attendance');
+    }
+};
---
+++ database/migrations/2023_08_23_161918_create_affiliation_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if(!Schema::hasTable('affiliation')){
+            Schema::create('affiliation', function (Blueprint $table) {
+                $table->id();
+                $table->integer('club_id')->unsigned();
+                $table->integer('member_id')->unsigned();
+                $table->text('role');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('affiliation');
+    }
+};
