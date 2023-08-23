6944192a5333ad18499f98c93c13aaf89075d628
toshima <tntoshima@gmail.com>
2023-08-23 18:45:48 +0900

    ハンバーガーメニューを追加

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,6 +1,35 @@
 <x-app-layout>
     <x-slot name="header">
         
+        <body>
+            <header class="header">
+            <!-- ヘッダーロゴ -->
+            <div class="logo">メニュー</div>
+            
+            <!-- ハンバーガーメニュー部分 -->
+            <div class="nav">
+            
+                <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+                <input id="drawer_input" class="drawer_hidden" type="checkbox">
+            
+                <!-- ハンバーガーアイコン -->
+                <label for="drawer_input" class="drawer_open"><span></span></label>
+            
+                <!-- メニュー -->
+                <nav class="nav_content">
+                <ul class="nav_list">
+                    <li class="nav_item"><a href="">ホーム</a></li>
+                    <li class="nav_item"><a href="">経費管理</a></li>
+                    <li class="nav_item"><a href="">カメラ</a></li>
+                    <li class="nav_item"><a href="">設定</a></li>
+                    <!--<li class="nav_item"><a href="">おしらせ</a></li>-->
+                    <!--<li class="nav_item"><a href="">チャット</a></li>-->
+                </ul>
+                </nav>
+        
+            </div>
+            </header>
+        </body>
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             @yield('page-name')
         </h2>
