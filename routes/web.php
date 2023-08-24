dc4b2fff7530746b924af9dd6310bc6ccda4b135
shuri0423 <d20119@numazu.kosen-ac.jp>
2023-08-24 11:47:07 +0900

    入力された値を計算して表示。

--- routes/web.php
+++ routes/web.php
@@ -43,6 +43,7 @@ Route::get('/payment', function () {
     return view('payment');
 });
 
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
