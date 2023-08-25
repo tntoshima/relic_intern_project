97392c2e99ee6920526bcd683ef9ba12dd9ca164
yamanaka <ao04to02@gmail.com>
2023-08-25 10:53:16 +0900

    QRmaka

--- resources/views/attendance/list.blade.php
+++ resources/views/attendance/list.blade.php
@@ -5,6 +5,26 @@
 @endsection
 
 @section('child')
+<meta charset="sjis">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<?php
+require_once("phpqrcode/qrlib.php");
+ 
+	// QR削除
+	array_map("unlink", glob("qr/sample.png"));
+ 
+	// 現在時刻取得
+	$date = date("Y-m-d");
+	$value = "現在時刻は{$date}です。";
+ 
+	// パスセット
+	$path = "phpqrcode/qr/sample.png";
+ 
+	$qr = new QRcode();
+ 
+	// QRコード作成
+	$qr->png($value, $path, QR_ECLEVEL_M, 12);
+?>
 
 <style>
     .table-container {
@@ -22,9 +42,18 @@
         border-bottom: 1px solid black;
         padding: 8px;
     }
-</style>
+    
+body 
+{
+	text-align: center;
+}
 
+</style>
+<h1><?php echo $value ?></h1>
+<body>
+<img  src="/phpqrcode/qr/sample.png"></body>
 <div class="table-container">
+
     <table>
         <tr>
             <th>生徒名</th>
